51110b377c2c77c52540c8482860dbb7cd6577d3
Marjori Fonte <marjorif@live.com>
2020-10-06 21:21:43 +0100

    Seeded 10 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating restaurants..."
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: [:chinese, :italian, :japanese, :french, :belgian].sample
+  )
+  puts "Restaurant #{restaurant.id} is created!"
+end
+
+puts "Tadá!"
